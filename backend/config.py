14e68c0e61141b60df3432b743440dd45793dd86
vladimir <vavakharitonov2004@gmail.com>
2025-05-13 22:02:40 +0300

    Revert "Add redis-db, mail, wrong put method create_user"
    This reverts commit d5113294d0a9d23cc684951ef5aebf41c4c7c422.

--- backend/config.py
+++ backend/config.py
@@ -6,7 +6,6 @@ from environs import Env
 @dataclass
 class Site:
     postgres: str
-    redis: str
 
 
 @dataclass
@@ -14,32 +13,20 @@ class JwtAuth:
     secret_key: str
     algorithm: str
 
-@dataclass
-class Mail:
-    mail_user: str
-    mail_password: str
-    to_email: str
 
 @dataclass
 class Config:
     site: Site
     jwt_auth: JwtAuth
-    mail: Mail
 
 
 def load_config():
     env = Env()
     env.read_env()
     return Config(
-        site=Site(env('DB_POSTGRES'),
-                  env('DB_REDIS')),
+        site=Site(env('DB_POSTGRES')),
         jwt_auth=JwtAuth(
             secret_key=env('SECRET_KEY_JWT'),
             algorithm=env("ALGORYTHM_JWT")
-        ),
-        mail=Mail(
-            mail_user=env('MAIL_USER'),
-            mail_password=env('MAIL_PASSWORD'),
-            to_email=env('TO_EMAIL')
         )
     )
--- backend/dependecies/redis_depends.py
+++
@@ -1,7 +0,0 @@
-from redis.asyncio import Redis
-
-from backend.database.redis_db import redis_client
-
-
-async def get_redis() -> Redis:
-    return redis_client
