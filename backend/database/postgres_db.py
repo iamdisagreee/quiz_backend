d5113294d0a9d23cc684951ef5aebf41c4c7c422
vladimir <vavakharitonov2004@gmail.com>
2025-05-13 21:58:49 +0300

    Add redis-db, mail, wrong put method create_user

--- backend/database/postgres_db.py
+++ backend/database/postgres_db.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
 from backend.config import load_config
+from backend.main import config
 
-config = load_config()
 engine = create_async_engine(config.site.postgres, echo=True)
 session_maker = async_sessionmaker(engine)
 
--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 
+from backend.config import load_config
 from backend.routers import auth
 
 app = FastAPI()
 app_v1 = FastAPI()
+config = load_config()
 
-app_v1.include_router(auth.router)
+app_v1.include_router(auth.router_v1)
 app.mount("/v1", app_v1)
